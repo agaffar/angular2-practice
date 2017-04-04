d6f5c587c37f19fed30a291c10a3be64f8b44df7
agaffar <bismillah@213>
2017-04-04 11:57:43 +0530

    intial

--- software/MyFirstApp/client1/my-dreamapp/src/app/components/home-component.component.ts
+++ software/MyFirstApp/client1/my-dreamapp/src/app/components/home-component.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'home',
-    template: require('./home-component.html')
+    template: `<h1>hello world</h1>`
 })
 export class HomeComponent {
     title: string = 'Home Page';
